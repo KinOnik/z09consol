fdf436c66dfdc6ef6fb90f0a59ccb939119fbfc6
KinOnik <84587417+KinOnik@users.noreply.github.com>
2021-12-13 17:45:29 +0300

    Add files via upload

---
+++ 1consol/1consol/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+
+namespace _1consol
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				string curPatch = Environment.CurrentDirectory;
+				Console.Write("\n\tДиапазон записываемых чисел (от): ");
+				double a = double.Parse(Console.ReadLine());
+				Console.Write("\n\tДиапазон записываемых чисел (до): ");
+				double b = double.Parse(Console.ReadLine());
+				if (a>b)
+                {
+					Console.Write("\n\n\t** Нарушен порядок ввода диапазона записываемых чисел, меняю местами **\n\n");
+					double buf = a;
+					a = b;
+					b= buf;
+                }
+				Console.Write("\n\tШаг чисел в диапазоне: ");
+				double h = double.Parse(Console.ReadLine());
+
+				FileStream f = new FileStream($"{curPatch}/tmp.dat", FileMode.Create);
+				BinaryWriter fOut = new BinaryWriter(f);
+				for (double i = a; i <= b; i += h)
+				{
+					fOut.Write(i);
+				}
+				fOut.Close();
+
+				f = new FileStream($"{curPatch}/tmp.dat", FileMode.Open);
+				BinaryReader fIn = new BinaryReader(f);
+				long m = f.Length;
+
+				Console.Write("\n\tДиапазон игнорируемых чисел (от): ");
+				double startIgnor = double.Parse(Console.ReadLine());
+				Console.Write("\n\tДиапазон игнорируемых чисел (до): ");
+				double stopIgnor = double.Parse(Console.ReadLine());
+
+				if (startIgnor>stopIgnor)
+				{
+					Console.Write("\n\n\t** Нарушен порядок ввода диапазона игнорируемых чисел, меняю местами **\n\n");
+					double buf = startIgnor;
+					startIgnor = stopIgnor;
+					stopIgnor = buf;
+				}
+				Console.Write("\n\n\t Вот числа с пропущенным диапазоном (пропущенные числа отмечены \'.\'):\n\t ");
+				double tmpDigit = 0;
+
+				for (long i = 0; i < m; i += 8)
+				{
+					f.Seek(i, SeekOrigin.Begin);
+					tmpDigit = Math.Round(fIn.ReadDouble(), 2);
+					if (tmpDigit >= startIgnor && tmpDigit <= stopIgnor)
+					{
+						Console.Write(".");
+					}
+					else
+					{
+						Console.Write($"{tmpDigit:f2} ");
+					}
+				}
+
+				Console.Write("\n\n");
+				fIn.Close();
+				f.Close();
+			}
+			catch (Exception)
+			{
+				Console.Write("\n**** Вы ввели не число ****\n");
+			}
+		}
+	}
+}
---
+++ 2consol/2consol/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace _2consol
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				string curPatch = Environment.CurrentDirectory;
+				Console.Write("k1= ");
+				int k1 = int.Parse(Console.ReadLine());
+				Console.Write("k2= ");
+				int k2 = int.Parse(Console.ReadLine());
+				if (k1 > k2)
+				{
+					Console.Write("\n\n\t** нарушены границы индексов элементов(k2>k1), меняю местами**\n\n");
+					int buffer = k1;
+					k1 = k2;
+					k2 = buffer;
+				}
+				string[] buf = System.IO.File.ReadAllLines($"{curPatch}/text.txt");
+				for (int i = 0; i < buf.Length; i++)
+				{
+					if (buf[i].Length >= k2)
+					{
+						Console.Write($"Символы строки №{i+1}: ");
+						for (int j = k1 - 1; j < k2; j++)
+						{
+							Console.Write($"{buf[i][j]}");
+						}
+						Console.Write($"\n");
+					}
+					else if (buf[i].Length >= k1 && buf[i].Length < k2)
+					{
+						Console.Write($"В строке №{i + 1} меньше символов, чем вы ввели : ");
+						for (int j = k1 - 1; j < buf[i].Length; j++)
+						{
+							Console.Write($"{buf[i][j]}");
+						}
+						Console.Write($"\n");
+					}
+					else
+					{
+						Console.Write($"В строке №{i + 1} нет символов с такими индексами в строке\n");
+					}					
+				}
+			}
+			catch (Exception)
+			{
+				Console.Write($"\n**** Вы ввели не число ****\n");
+			}
+
+		}
+	}
+}
